f10a3ce1ed7508a311b2d43238ea9adaca6fec9c
Aryadne Cunha <aryadnecunha@gmail.com>
2026-01-18 19:16:44 -0300

    mais operadores lógicos

--- FrontEnd/modulo_javascript_basico/05-operadores-logicos/index.js
+++ FrontEnd/modulo_javascript_basico/05-operadores-logicos/index.js
@@ -16,7 +16,66 @@
     < ---- maior igual que ---- a <= b ---- verdadeiro quando a for maior ou igual que b
 */
 
-const a = 3
-const b = 3
+//const a = 3
+//const b = 3
 
-console.log(a==b)
+//console.log(a==b)
+
+////////////////////////////////
+
+/*
+    && = e
+    T e T = T
+    T e F = F
+    F e T = F
+    F e F = F
+*/
+
+const a = 2
+const b = 2
+
+
+console.log(a === b && a <=b)
+//V e V = V
+
+console.log(a === b && a < b)
+//V e F = F
+
+console.log(a > b && a === b)
+//F e V = F
+
+console.log(a > b && a < b)
+//F e F = F
+
+
+/*
+    || = OR = ou
+    T e T = T
+    T e F = T
+    F e T = T
+    F e F = F
+*/
+
+console.log(a === b || a <= b)
+//V or V = V
+
+console.log(a === b || a < b)
+//V or F = V
+
+console.log(a > b || a === b)
+//F or V = V
+
+console.log(a > b || a < b)
+//F or F = F
+
+/*
+    ! = not = inverter os valores
+    T fica F 
+    F fica T
+*/
+
+console.log(a === b ) //true
+console.log(!(a === b)) //false
+
+console.log(a < b ) //false
+console.log(!(a < b)) //true
