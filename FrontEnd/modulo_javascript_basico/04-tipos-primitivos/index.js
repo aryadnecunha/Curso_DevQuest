c55b0526ac89f4e4fc5267ad3ed90fd504373f37
Aryadne Cunha <aryadnecunha@gmail.com>
2026-01-18 18:58:12 -0300

    operadores lógicos

---
+++ FrontEnd/modulo_javascript_basico/04-tipos-primitivos/index.js
@@ -0,0 +1,20 @@
+/*
+    String
+    Number
+    Boolean
+    Null
+    Undefined
+*/
+
+let nome = "Aryadne"
+let numero = 123
+let usuarioVerificado = true
+
+if(usuarioVerificado){
+    console.log("usuario verificado")
+}else{
+    console.log("usuario não é verificado")
+}
+
+let pessoa = null
+let telefone = undefined
---
+++ FrontEnd/modulo_javascript_basico/05-operadores-logicos/index.js
@@ -0,0 +1,22 @@
+/*
+    == ---- igual ---- a==b ---- verdadeiro se a for igual a b
+
+    === ---- idêntico ---- a===b ---- verdadeiro se a for idêntico a b
+
+    != ---- diferente ---- a!=b ---- verdadeiro se a não for idêntico a b
+    
+    !== ---- não idêntico ---- a!== ---- verdadeiro se a não for idêntico a b
+
+    < ---- menor que ---- a < b ---- verdadeiro quando a for menor que b
+
+    < ---- menor igual que ---- a <= b ---- verdadeiro quando a for menor ou igual que b
+
+    > ---- maior que ---- a > b ---- verdadeiro quando a for maior que b
+
+    < ---- maior igual que ---- a <= b ---- verdadeiro quando a for maior ou igual que b
+*/
+
+const a = 3
+const b = 3
+
+console.log(a==b)
