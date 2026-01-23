4618e15341b0692a1d5eb245f4f0a6ee9c7cb156
Aryadne Cunha <aryadnecunha@gmail.com>
2026-01-23 20:39:58 -0300

    arrow functions

---
+++ FrontEnd/modulo_javascript_intermediario/04-arrow-funcions/index.js
@@ -0,0 +1,20 @@
+/*
+ let resultadoDaSoma = (numero1, numero2) =>{
+    return numero1 + numero2
+}
+
+console.log(resultadoDaSoma(12, 5))
+*/
+
+/*
+const incentivarQuest = nomeQuester => {
+    console.log('Parabéns ' + nomeQuester + ', você chegou ao módulo de JS intermediário')
+} 
+
+incentivarQuest('Aryadne')
+*/
+
+
+const incentivarQuest = nomeQuester => console.log('Parabéns ' + nomeQuester + ', você chegou ao módulo de JS intermediário')
+
+incentivarQuest('Aryadne')
